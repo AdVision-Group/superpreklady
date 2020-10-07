2f425c645e3894872f20d31a6307709c1a8e4b9f
MRKSC <sadecky.marek@gmail.com>
2020-10-07 09:57:49 +0200

    Updates

---
+++ precache-manifest.7fb3c3db3578521161a0ef90fe4447aa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7860fff90abca104627e1c64efbcaa62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be5d58e153f278f1f88a",
+    "url": "/static/css/main.506712cb.chunk.css"
+  },
+  {
+    "revision": "ab3b25a6490e14cdd09a",
+    "url": "/static/js/2.7917ce30.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.7917ce30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be5d58e153f278f1f88a",
+    "url": "/static/js/main.362cc3f5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "ef6da18b9dce32750807693b631b6b84",
+    "url": "/static/media/article-black-18dp.ef6da18b.svg"
+  },
+  {
+    "revision": "b4175d5ac27589aa2231e42f1e4fd852",
+    "url": "/static/media/phone-black-18dp.b4175d5a.svg"
+  },
+  {
+    "revision": "6fac293e2bdfb212031f4867a6a8df4a",
+    "url": "/static/media/superpreklady-about-image.6fac293e.jpg"
+  },
+  {
+    "revision": "9c1fdafa4983462905ec1b1b3f00567a",
+    "url": "/static/media/superpreklady-icon-home.9c1fdafa.png"
+  }
+]);
